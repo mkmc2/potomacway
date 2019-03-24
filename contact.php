247c58c078be57b08f92114594bd09933c694add
Matthew McLaughlin <mkmclaughlin2@gmail.com>
2019-03-24 09:22:04 -0400

    email address updates

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 if($_POST["message"]) {
-    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+    mail("mkmclaughlin2@gmail.com", "Form to email message", $_POST["message"], "From: potomacway");
 }
 ?>
 
